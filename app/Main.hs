e57eeaaacbcef52e09ddd4d23c7ce8177f0750d8
Krantz-XRF <Krantz.XRF@outlook.com>
2019-07-23 19:18:30 +0800

    Finish the executable for extracting outline glyphs as SVG images - Ability to save as SVG files or to print to stdout. - Ability to choose the character to extract.

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,13 @@
 {-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE LambdaCase #-}
 module Main where
 
+import Foreign.Storable (peek)
+import Data.Maybe (listToMaybe)
+
 import Control.Monad (mapM_, when)
-import System.Exit (exitWith, ExitCode(..))
+import System.Exit (exitSuccess)
+import System.IO (withFile, IOMode(..), FilePath, Handle, stdout)
 
 import Text.Read (readMaybe)
 import Text.Printf (printf)
@@ -11,6 +16,8 @@ import Options
 import FreeType.Library
 import FreeType.Face
 import FreeType.Glyph
+import FreeType.Outline
+import FreeType.Types
 
 defaultDebugLevel :: Int
 defaultDebugLevel = 2
@@ -19,6 +26,8 @@ data Options = Options
     { printVersion :: Bool
     , helpMessage :: Bool
     , debugOutput :: Int
+    , outputFile :: Maybe String
+    , extractChar :: Maybe Char
     } deriving (Show)
 
 defaultOptions :: Options
@@ -26,13 +35,15 @@ defaultOptions = Options
     { printVersion = False
     , helpMessage = False
     , debugOutput = 0
+    , outputFile = Nothing
+    , extractChar = Nothing
     }
 
 options :: [OptDescr String Options]
 options =
-    [ Option ['V'] ["version"] "Print version number"
+    [ Option ['V'] ["version"] "Print version number."
         $ NoArg $ \opt -> return opt{ printVersion = True }
-    , Option ['h'] ["help"] "Print this help message"
+    , Option ['h'] ["help"] "Print this help message."
         $ NoArg $ \opt -> return opt{ helpMessage = True }
     , Option [] ["debug"] "Set debug level."
         $ OptArg "debug level"
@@ -41,6 +52,12 @@ options =
                 Just s -> case readMaybe s of
                     Nothing -> Left "--debug need a number as debug level"
                     Just lv -> return opt{ debugOutput = lv }
+    , Option ['o'] ["output"] "Set output file."
+        $ ReqArg "output file"
+        $ \str opt -> return opt{ outputFile = Just str }
+    , Option ['c'] ["extract", "char"] "Select the character to extract."
+        $ ReqArg "char"
+        $ \str opt -> return opt{ extractChar = listToMaybe str }
     ]
 
 main :: IO ()
@@ -53,16 +70,33 @@ main = do
         Right Options{..} -> do
             when (null rest || printVersion) $ putStrLn "FreeType-convert 2019"
             when (null rest || helpMessage) $ putStrLn $ usageInfo "Usage:" options
-            when (printVersion || helpMessage) $ exitWith ExitSuccess
-            mapM_ mainProc rest
+            when (printVersion || helpMessage) exitSuccess
+            case rest of
+                [] -> putStrLn "No input file."
+                (f:_) -> case extractChar of
+                    Nothing -> putStrLn "No character to extract."
+                    Just c -> mainProc f c outputFile
 
-mainProc :: String -> IO ()
-mainProc path =
+withFileOr :: Maybe FilePath -> IOMode -> Handle -> (Handle -> IO a) -> IO a
+withFileOr Nothing  _ h proc = proc h
+withFileOr (Just f) m _ proc = withFile f m proc
+
+mainProc :: String -> Char -> Maybe FilePath -> IO ()
+mainProc path ch outPath =
     withFreeType $ \lib ->
     withFace lib path 0 $ \face -> do
         setCharSize face 0 16.0 0 0
         putStrLn "FreeType: Ready."
         printf "Font face loaded: %s (%s)\n" path (show face)
-        withCharGlyph face 'A' [LoadNoBitmap] $ \g -> case g of
-            Nothing -> putStrLn "Failed to load glyph of 'A'."
-            Just g' -> printf "Glyph loaded: A (%s)\n" (show g')
+        withCharGlyph face ch [LoadNoBitmap] $ \case
+            Nothing -> printf "Failed to load glyph of '%c'." ch
+            Just g -> do
+                printf "Glyph loaded: '%c' (%s)\n" ch (show g)
+                mog <- castOutlineGlyph g
+                case mog of
+                    Nothing -> putStrLn "Unexpected glyph format."
+                    Just og -> withFileOr outPath WriteMode stdout $ \outFile -> do
+                        putStrLn "Outline glyph confirmed."
+                        let po = c_outline og
+                        outlineTransform po (Matrix 1 0 0 (-1))
+                        printOutlineSVG "black" outFile po
